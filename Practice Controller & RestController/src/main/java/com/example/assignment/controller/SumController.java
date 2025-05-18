c0f9d4ff0c5e5998201d0446061fd8a94d7cb105
mechatemesgen <ililnaafbarihe94@gmail.com>
2025-05-18 12:23:44 +0300

    Remove unused import statement from SumController

--- Practice Controller & RestController/src/main/java/com/example/assignment/controller/SumController.java
+++ Practice Controller & RestController/src/main/java/com/example/assignment/controller/SumController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.ui.Model;
-import java.util.Map;
 
 @Controller
 public class SumController {
